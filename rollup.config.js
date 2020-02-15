9dd8cd91d50420e47d5114701c2d6a9106723fbd
SyntaxRules <devyn.stott@gmail.com>
2020-02-15 10:01:36 -0700

    chore(bulma): init the bulma + svelte combo

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
+import postcss from 'rollup-plugin-postcss';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -24,7 +25,8 @@ export default {
 				css.write('public/build/bundle.css');
 			}
 		}),
-
+		//added for bulma
+		postcss(),
 		// If you have external dependencies installed from
 		// npm, you'll most likely need these plugins. In
 		// some cases you'll need additional configuration -
--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,27 @@
 <script>
-	export let name;
+	import './main.scss'
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+<section class="hero is-fullheight">
+  <div class="hero-head">
+  </div>
+
+  <div class="hero-body">
+    <div class="container has-text-centered">
+      <p class="title">
+        Faros Education
+      </p>
+      <p class="subtitle">
+        Revitalizing higher-education technology
+      </p>
+    </div>
+  </div>
+
+  <div class="hero-foot">
+  </div>
+</section>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
